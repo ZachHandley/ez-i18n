6b0197383cff80b9a26f137c59f153b7e238e8d9
Zach Handley <zachhandley@gmail.com>
2026-01-13 14:54:49 -0800

    add update-versions script

---
+++ scripts/update-versions.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const args = process.argv.slice(2);
+const packagePath = args[0];
+const targetVersion = args[1];
+const updatePeer = args.includes('--update-peer');
+
+if (!packagePath || !targetVersion) {
+  console.error('Usage: update-versions.js <packagePath> <version> [--update-peer]');
+  process.exit(1);
+}
+
+const pkgPath = path.join(packagePath, 'package.json');
+const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
+
+pkg.version = targetVersion;
+
+if (updatePeer && pkg.peerDependencies && pkg.peerDependencies['@zachhandley/ez-i18n']) {
+  pkg.peerDependencies['@zachhandley/ez-i18n'] = `>=${targetVersion}`;
+}
+
+fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
